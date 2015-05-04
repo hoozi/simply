1775a443f9947b742459e74d57a6ec70f508e1dd
hoozi <287036406@qq.com>
2015-05-04 15:35:03 +0800

    add route

--- simply/libs/App.js
+++ simply/libs/App.js
@@ -13,65 +13,59 @@ function App() {
         that = this;
         
     /*     
-     * get method监听器
+     * 用于储存get监听器
      * */
-    this._getHandle = null
+    this._route_getHandle = {}
     
     /*     
-     * post method监听器
+     * 用于储存post监听器
      * */
-    this._postHandle = null
+    this._route_postHandle = {}
+
      
     function serverHandle(req, res) { 
         
-        if(!middleList) {
-            
-        } else {
-            var middleIndex = 0;
-            
+        var middleIndex = 0;
+        
+        execMiddle();
+        
+        /*
+         * next方法在中间件内部执行，表示当前中间件执行完毕后，执行下一个中间件;
+         * */
+        function next() {
+            middleIndex++;
             execMiddle();
+        }
+        
+        /*
+         * 执行当前中间件;
+         * */
+        function execMiddle() {
+            var middle = middleList[middleIndex];
             
             /*
-             * next方法在中间件内部执行，表示当前中间件执行完毕后，执行下一个中间件;
-             * */
-            function next() {
-                middleIndex++;
-                execMiddle();
-            }
-            
-            /*
-             * 执行当前中间件;
-             * */
-            function execMiddle() {
-                var middle = middleList[middleIndex];
+             * 判断是否存在中间件
+             * */  
+            if(middle) {
                 
                 /*
-                 * 判断是否存在中间件
-                 * */  
-                if(middle) {
-                    
-                    /*
-                     * next以参数形式传入中间件的内部，在中间件内部执行；
-                     * */
-                    middle(req, res, next);
-                } else {
-                    var method = req.method;
-                    switch(method) {
-                       case "GET":
-                           if(that._getHandle) {
-                                that._getHandle(req, res)
-                           }
-                       break;
-                       case "POST":
-                           if(that._postHandle) {
-                               that._postHandle(req, res)
-                           }
-                       break; 
-                    }
+                 * next以参数形式传入中间件的内部，在中间件内部执行；
+                 * */
+                middle(req, res, next);
+            } else {
+                var method = req.method,handle
+                switch(method) {
+                   case "GET":
+                       handle = that._route_getHandle[req.url];
+                   break;
+                   case "POST":
+                       handle = that._route_postHandle[req.url];
+                   break; 
+                }
+                if(handle) {
+                    handle(req, res)
                 }
             }
-            
-            
         }
     }
     this._server = http.createServer(serverHandle);
@@ -88,11 +82,11 @@ App.prototype.listen = function() {
 }
 
 //get method
-App.prototype.get = function(handle) {
-    this._getHandle = handle;
+App.prototype.get = function(route, handle) {
+    this._route_getHandle[route] = handle;
 }
 
 //post method
-App.prototype.post = function(handle) {
-    this._postHandle = handle;
+App.prototype.post = function(route, handle) {
+    this._route_postHandle[route] = handle;
 }
